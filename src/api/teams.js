c290bd32876cf8a32ef1ec4a30ada94158a12716
Viljami Kuosmanen <anttiviljami@futurice.com>
2018-05-20 01:31:38 +0300

    Input data

--- src/api/teams.js
+++ src/api/teams.js
@@ -1,9 +1,138 @@
 export const teams = [
   {
-    title: 'Oleary',
-    picks: {
-      company: 'Chevron',
-      ticker: 'CVX',
-    }
-  }
+    name: 'Stevens Institute Investment Club',
+    picks: [
+      {
+        company: 'Facebook',
+        ticker: 'FB',
+      },
+      {
+        company: 'Disney',
+        ticker: 'DIS',
+      },
+      {
+        company: 'SAGE Therapeutics',
+        ticker: 'SAGE',
+      },
+    ],
+  },
+  {
+    name: 'Todd Gordon',
+    picks: [
+      {
+        company: 'Bitcoin',
+        ticker: 'BTX',
+      },
+      {
+        company: 'Nvidia',
+        ticker: 'NVDA',
+      },
+      {
+        company: 'Netflix',
+        ticker: 'NFLX',
+      },
+    ],
+  },
+  {
+    name: 'Kevin O\'Leary',
+    picks: [
+      {
+        company: 'Chevron',
+        ticker: 'CVX',
+      },
+      {
+        company: 'Blue Apron',
+        ticker: 'APRN',
+      },
+      {
+        company: 'Boeing',
+        ticker: 'BA',
+      },
+    ],
+  },
+  {
+    name: 'The Beardstown Ladies',
+    picks: [
+      {
+        company: 'Celgene',
+        ticker: 'CELG',
+      },
+      {
+        company: 'Apple',
+        ticker: 'AAPL',
+      },
+      {
+        company: 'Visa',
+        ticker: 'V',
+      },
+    ],
+  },
+  {
+    name: 'Eric Dickerson',
+    picks: [
+      {
+        company: 'Exxon Mobil',
+        ticker: 'XOM',
+      },
+      {
+        company: 'Anadarko Petroleum',
+        ticker: 'APC',
+      },
+      {
+        company: 'Oil',
+        ticker: '@LCO.1',
+      },
+    ],
+  },
+  {
+    name: 'Tim Seymour',
+    picks: [
+      {
+        company: 'Amgen',
+        ticker: 'AMGN',
+      },
+      {
+        company: 'Dropbox',
+        ticker: 'DBX',
+      },
+      {
+        company: 'Chesapeake Energy',
+        ticker:  'CHK',
+      },
+    ],
+  },
+  {
+    name: 'Nick Lowery',
+    picks: [
+      {
+        company: 'Amazon',
+        ticker: 'AMZN',
+      },
+      {
+        company: 'Goldman Sachs',
+        ticker: 'GS',
+      },
+      {
+        company: 'AMD',
+        ticker: 'AMD',
+      },
+    ],
+  },
+  {
+    name: 'Bethenny Frankel',
+    picks: [
+      {
+        company: 'General Electric',
+        ticker: 'GE',
+      },
+      {
+        company: 'Spotify',
+        ticker: 'SPOT',
+      },
+      {
+        company: 'Alphabet',
+        ticker: 'GOOGL',
+      },
+    ],
+  },
 ];
--- src/components/Team.js
+++ src/components/Team.js
@@ -5,7 +5,12 @@ export default class Team extends Component {
     const { team } = this.props;
     return (
       <div className="team">
-        <h2>{ team.title }</h2>
+        <h2>{ team.name }</h2>
+        { team.picks.map(pick => 
+          (<pre key={pick.ticker}>
+            { JSON.stringify(pick, null, 2) }
+          </pre>)
+        )}
       </div>
     );
   }
