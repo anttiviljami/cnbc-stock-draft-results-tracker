2c9219107ef99a59e91c860a83851d29e8415d48
Viljami Kuosmanen <anttiviljami@futurice.com>
2018-05-20 02:59:05 +0300

    Input starting prices for stocks

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+import Promise from 'bluebird';
 import _ from 'lodash';
 import alphaVantage from 'alphavantage';
 
@@ -18,10 +19,24 @@ export async function apiHandler(req, res) {
       const { symbol } = stock;
       const quote = _.find(quotes, { '1. symbol': stock.symbol });
       const stockPrice = _.get(quote, '2. price', 0);
-      const price = symbol === 'BTX' ? btcPrice : stockPrice;
+      const price = symbol === 'BTX' ? Number(btcPrice) : Number(stockPrice);
       return { ...stock, price };
     })
     return { ...team, picks: picksWithPrices };
   });
   res.send({ teams: teamsWithPrices });
+}
+
+export async function getStartingPrices() {
+  const stocks = _.flatMap(teams, 'picks');
+  const prices = await Promise.map(stocks, async (stock) => 
+    alpha.data.daily(stock.symbol).then(res => {
+      console.log('Fetched', stock.symbol);
+      const timeSeries = _.get(res, 'Time Series (Daily)', {});
+      const day = timeSeries['2018-04-26'] || {};
+      const close = Number(_.get(day, '4. close', 0));
+      return { symbol: stock.symbol, close }
+    })
+  , { concurrency: 3 });
+  return prices;
 }
--- src/api/teams.js
+++ src/api/teams.js
@@ -5,14 +5,17 @@ export const teams = [
       {
         company: 'Facebook',
         symbol: 'FB',
+        close: 174.16,
       },
       {
         company: 'Disney',
         symbol: 'DIS',
+        close: 99.84,
       },
       {
         company: 'SAGE Therapeutics',
         symbol: 'SAGE',
+        close: 143.66,
       },
     ],
   },
@@ -22,14 +25,17 @@ export const teams = [
       {
         company: 'Bitcoin',
         symbol: 'BTX',
+        close: 0,
       },
       {
         company: 'Nvidia',
         symbol: 'NVDA',
+        close: 225.22
       },
       {
         company: 'Netflix',
         symbol: 'NFLX',
+        close: 313.98,
       },
     ],
   },
@@ -39,14 +45,17 @@ export const teams = [
       {
         company: 'Chevron',
         symbol: 'CVX',
+        close: 124.22,
       },
       {
         company: 'Blue Apron',
         symbol: 'APRN',
+        close: 2.26,
       },
       {
         company: 'Boeing',
         symbol: 'BA',
+        close: 342.79,
       },
     ],
   },
@@ -56,14 +65,17 @@ export const teams = [
       {
         company: 'Celgene',
         symbol: 'CELG',
+        close: 92.08,
       },
       {
         company: 'Apple',
         symbol: 'AAPL',
+        close: 164.22,
       },
       {
         company: 'Visa',
         symbol: 'V',
+        oclose: 127.08,
       },
     ],
   },
@@ -73,14 +85,17 @@ export const teams = [
       {
         company: 'Exxon Mobil',
         symbol: 'XOM',
+        close: 80.86,
       },
       {
         company: 'Anadarko Petroleum',
         symbol: 'APC',
+        close: 67.5,
       },
       {
         company: 'Crude Oil',
         symbol: 'BNO',
+        close:  20.59,
       },
     ],
   },
@@ -90,14 +105,17 @@ export const teams = [
       {
         company: 'Amgen',
         symbol: 'AMGN',
+        close: 175.37,
       },
       {
         company: 'Dropbox',
         symbol: 'DBX',
+        close: 29.33,
       },
       {
         company: 'Chesapeake Energy',
         symbol:  'CHK',
+        close: 3.02,
       },
     ],
   },
@@ -107,14 +125,17 @@ export const teams = [
       {
         company: 'Amazon',
         symbol: 'AMZN',
+        close: 1517.96,
       },
       {
         company: 'Goldman Sachs',
         symbol: 'GS',
+        close: 240.09,
       },
       {
         company: 'AMD',
         symbol: 'AMD',
+        close: 11.04,
       },
     ],
   },
@@ -124,14 +145,17 @@ export const teams = [
       {
         company: 'General Electric',
         symbol: 'GE',
+        close: 14.38,
       },
       {
         company: 'Spotify',
         symbol: 'SPOT',
+        close: 156.6,
       },
       {
         company: 'Alphabet',
         symbol: 'GOOGL',
+        close: 1043.31,
       },
     ],
   },
