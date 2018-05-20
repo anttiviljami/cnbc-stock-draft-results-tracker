a3e1ed282a33d2fcda4cf167bbbbd916a3c5e7ca
Viljami Kuosmanen <anttiviljami@futurice.com>
2018-05-20 13:43:22 +0300

    Add description

--- src/App.js
+++ src/App.js
@@ -29,9 +29,15 @@ class App extends Component {
       <section>
         <header>
           <figure>
-            <h1>2018 CNBC Stock Draft Standings</h1>
+            <h1><span role="img" aria-label="stock">📈</span> 2018 CNBC Stock Draft Live Standings</h1>
+            <blockquote>
+              <strong>What is this?</strong> &mdash; This is a fan-made, non-official live tracker website for the CNBC 2018 Stock Draft.
+              More info about the Stock Draft and participating teams available on the
+              <a href="https://www.cnbc.com/2018/04/26/heres-a-live-look-at-power-lunchs-2018-cnbc-stock-draft-results.html"
+                target="_blank"
+                rel="noopener noreferrer"> CNBC website</a>.
+            </blockquote>
           </figure>
-          <blockquote>Standings are based on companies performance, not including dividends, from the stock's closing price on Apr. 26, 2018</blockquote>
         </header>
         { teams.length > 0 ? 
           ( teams.map((team, index) => <Team key={team.name} team={team} standing={index + 1} />) ) : 
@@ -41,12 +47,17 @@ class App extends Component {
           </article>
           )}
         <footer>
+          <blockquote>Standings are based on companies performance, not including dividends, from the stock's closing price on Apr. 26, 2018</blockquote>
           <p>
             <span>Stock data from <a href="https://www.alphavantage.co" target="_blank">Alpha Vantage API</a></span>
             <span style={{ float: 'right' }}>
-              <a href="https://github.com/anttiviljami/cnbc-stock-draft-results-tracker" target="_blank" rel="noopener noreferrer">Project GitHub page</a>,
+              <a href="https://github.com/anttiviljami/cnbc-stock-draft-results-tracker"
+                target="_blank"
+                rel="noopener noreferrer">Project GitHub page</a>,
               Created by&nbsp;
-              <a href="https://twitter.com/anttiviljami" target="_blank" rel="noopener noreferrer">@anttiviljami</a>
+              <a href="https://twitter.com/anttiviljami"
+                target="_blank"
+                rel="noopener noreferrer">@anttiviljami</a>
             </span>
           </p>
         </footer>
